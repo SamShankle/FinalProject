6777791d7c52b6fca9978e36fd0bbe5fa89305e8
Sam Shankle <118214783+SamShankle@users.noreply.github.com>
2023-04-17 13:44:48 -0500

    Update controller.sv

--- SV/controller.sv
+++ SV/controller.sv
@@ -7,14 +7,14 @@ output logic out;
 
 typedef enum logic {S0, S1} statetype;
 
-statetype state, nextstate;
+statetype currentstate, nextstate;
 
 always_ff @(posedge clk, posedge reset)
-    if (reset) state <=  S0;
-    else state <= nextstate;
+    if (reset) currentstate <=  S0;
+    else currentstate <= nextstate;
 
 always_comb 
-    case(state)
+    case(currentstate)
         S0: begin
             out <= 1'b0;
             if (a) nextstate <= S1;
@@ -22,13 +22,28 @@ always_comb
         end
         S1: begin
             out <= 1'b1;
-            if(a) nextstate <= S0;
-            else nextstate <= S0
+            if(a) nextstate <= S1;
+            else nextstate <= S0;
         end
 endcase
 
 endmodule
 
-module mux();
-  assign c = sel ? a : b;
+module MUX(in0, in1, grid_e, s);
+    output logic [63:0] grid_e;
+    input logic[63:0] in0, in1;
+    input logic s;
+    assign grid_e = s ? in1 : in0;
+endmodule
+
+module gameStart(init, inp0, inp1, outp);
+    input logic init;
+    input logic[63:0] inp0, inp1;
+    output logic[63:0] outp;
+    logic line;
+    
+    TB tb(.start(a));
+    FSM fsm(.a(init), .out(line));
+    MUX mux(.s(line), .in0(inp0), .in1(inp1), .grid_e(outp));
+    datapath dp(.grid(grid_evolve), .grid_evolve(grid_e))
 endmodule
