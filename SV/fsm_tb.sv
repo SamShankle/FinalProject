8eff8dff9aef477c69784f465b91114916d3be59
Sam Shankle <118214783+SamShankle@users.noreply.github.com>
2023-04-19 19:30:30 -0500

    Update fsm_tb.sv

--- SV/fsm_tb.sv
+++ SV/fsm_tb.sv
@@ -1,60 +1,48 @@
 `timescale 1ns / 1ps
-module tb ();
-    logic clk;
-    logic reset;
-    logic a;
-    logic out;
-    logic seed;
-    logic start;
+module tb;
+   logic start;
+   reg clk;
+   reg reset;
+   wire [63:0] seed;
+   wire [63:0] grid_e;
+   
+   // Instantiate DUT
+   FSM dut (
+      .clk(clk),
+      .reset(reset),
+      .a(start),
+      .out()
 
-    integer handle3;
-    integer desc3;
-
-    FSM dut(clk, seed, start, reset, a, out);
-    initial 
-    begin
-    clk = 1'b1;
-    forever #5 clk = ~clk;    
-    end
-    
-    initial
-    begin
-    handle3 = $fopen("fsm.out");
-    #500 $finish;
-    end
-
-    always 
-    begin
-    desc3 = handle3;
-    #10 $fdisplay(desc3, "RST = %b | A = %b || Out = %b", reset, a, out);
-    end
-
-    initial
-    begin 
-    #0  reset = 1'b1;
-    #0  a = 1'b0;
-    #10 reset = 1'b0;
-
-    #0  a = 1'b0;
-    #10 a = 1'b1;
-    #40 a = 1'b0;
-    #0  reset = 1'b0;
-    #10 reset = 1'b1;
-    #10 reset = 1'b0;
-
-    #0  a = 1'b0;
-    #10 a = 1'b1;
-    #40 a = 1'b0;
-    #0  reset = 1'b0;
-    #10 reset = 1'b1;
-    #10 reset = 1'b0;
-
-    #0  a = 1'b0;
-    #10 a = 1'b1;
-    #40 a = 1'b0;
-    #0  reset = 1'b0;
-    #10 reset = 1'b1;
-    #10 reset = 1'b0;
-    end
-    
+   );
+   MUX dut2(
+      .in0(seed),
+      .in1(),
+      .grid_e(),
+      .s()
+   );
+   
+   // Generate clock
+   initial clk = 0;
+   always #5 clk = ~clk;
+   
+   // Add reset
+   initial begin
+      reset = 1;
+      #10 reset = 0;
+      #10 start = 1'b0; 
+      #40 start = 1'b1;
+   end
+   
+   // Add testbench stimulus
+   initial begin
+      #20 $display("Grid: %b", seed);
+      #20 $display("Grid Evolve: %b", grid_e);
+      #30 $display("Grid: %b", seed);
+      #30 $display("Grid Evolve: %b", grid_e);
+      #40 $display("Grid: %b", seed);
+      #40 $display("Grid Evolve: %b", grid_e);
+      #50 $finish;
+   end
+   
 endmodule
+
