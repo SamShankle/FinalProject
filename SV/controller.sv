01706cf5bf852e7478b39a8dfa12597d13c7a269
Sam Shankle <118214783+SamShankle@users.noreply.github.com>
2023-04-27 14:06:13 -0500

    Update controller.sv

--- SV/controller.sv
+++ SV/controller.sv
@@ -41,16 +41,25 @@ module MUX(in0, in1, grid_e, s);
     assign grid_e = s ? in1 : in0;
 endmodule
 
-module gameStart(start, seed, clk, reset, outp);
+module gameStart(start, seed, clk, reset, toHDMI);
     input logic start;
     input logic[63:0] seed;
-    output logic[63:0] outp;
+    output logic[63:0] toHDMI;
     logic line; 
-    logic [63:0] grid_evolve;
+    logic [63:0] grid_evolve, outp;
     input logic clk, reset;
     
     
     FSM fsm(.a(start), .clk(clk), .reset(reset), .out(line));
-    MUX mux(.s(line), .in0(seed), .in1(grid_evolve), .grid_e(outp));
+    MUX mux(.s(line), .in0(seed), .in1(toHDMI), .grid_e(outp));
     datapath dp(.grid(outp), .grid_evolve(grid_evolve));
+    register Reggi(.D(grid_evolve), .clk(clk), .reset(reset), .Q(toHDMI)); // Holds output until clk
+endmodule
+
+module register(input logic [63:0] D, input logic clk, reset, output logic [63:0] Q);
+    always @(posedge clk, posedge reset) begin
+        if (reset) Q <= 64'h0;
+        else Q <= D;
+
+    end
 endmodule
